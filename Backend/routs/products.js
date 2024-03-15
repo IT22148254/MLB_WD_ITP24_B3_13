de3aade4277d94302d5f69240aef419a51db7109
Beelzebub <bleezbub12@gmail.com>
2024-03-15 11:05:42 +0530

    First commit on praboth

---
+++ Backend/routs/products.js
@@ -0,0 +1,71 @@
+const router = require("express").Router();
+let Product = require("../models/product");
+
+router.route("/add").post((req, res) => {
+  const { Name, Price, Description, Stock, Sold } = req.body;
+
+  const newProduct = new Product({
+    Name,
+    Price,
+    Description,
+    Stock,
+    Sold,
+  });
+
+  newProduct
+    .save()
+    .then(() => {
+      res.status(200).json({ message: "Product added successfully" });
+    })
+    .catch((err) => {
+      res
+        .status(400)
+        .json({ message: "Something went wrong when adding the product" });
+    });
+});
+
+router.route("/").get((req, res) => {
+  Product.find()
+    .then((products) => {
+      res.status(200).json(products);
+    })
+    .catch((err) => {
+      res
+        .status(400)
+        .json({ err: `${err}Something went wrong while fetching all products` });
+    });
+});
+
+router.route("/:id").put((req, res) => {
+  const id = req.params.id;
+  const { Name, Price, Description } = req.body;
+
+  Product.findByIdAndUpdate(id, { Name, Price, Description }, { new: true })
+    .then((result) => {
+      if (!result) {
+        res.status(404).json("Product not found");
+      }
+
+      res.status(200).json("Product updated successfully");
+    })
+    .catch((err) => {
+      res.status(400).json(`${err} : Product update unsuccessful`);
+    });
+});
+
+router.route("/:id").delete((req, res) => {
+  const id = req.params.id;
+
+  Product.findByIdAndDelete(id)
+    .then((result) => {
+      if (!result) {
+        res.status(404).json("Product not found");
+      }
+      res.status(200).json("Prduct removed successfully");
+    })
+    .catch((err) => {
+      res.status(400).json(`${err} : Product removal unsuccessful`);
+    });
+});
+
+module.exports = router;
---
+++ Backend/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+require("dotenv").config();
+const app = express();
+
+const PORT = process.env.PORT || 8070;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, {});
+
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("Mongodb connection successful");
+});
+
+const productRouter = require("./routs/products.js");
+app.use("/product", productRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is up and running on port : ${PORT}`);
+});
