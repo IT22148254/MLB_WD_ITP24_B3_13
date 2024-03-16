a1c09ebf0aad939410adadf4a5395ea0323cac33
Beelzebub <bleezbub12@gmail.com>
2024-03-16 19:54:13 +0530

    renamed the url in .env

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 8070;
 app.use(cors());
 app.use(bodyParser.json());
 
-const URL = process.env.MONGODB_URL;
+const URL = process.env.MONGODB_URL_STORE;
 
 mongoose.connect(URL, {});
 
