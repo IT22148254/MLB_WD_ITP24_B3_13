b026421117a394206f1eaec75f2d32c4ada85bdf
Beelzebub <bleezbub12@gmail.com>
2024-03-19 13:23:29 +0530

    feed back model complete

--- Backend/models/feedback.js
+++ Backend/models/feedback.js
@@ -1,5 +1,20 @@
 const mongoose = require("mongoose");
 const schema = mongoose.Schema();
 
+const feedBackSchema = new schema({
+  UserName: {
+    type: String,
+    required: true,
+  },
+  Comment: {
+    type: String,
+    required: true,
+  },
+  Rating: {
+    type: Number,
+    required: true,
+  },
+});
 
-
+const FeedBack = mongoose.model("FeedBack", feedBackSchema);
+module.exports = FeedBack;
