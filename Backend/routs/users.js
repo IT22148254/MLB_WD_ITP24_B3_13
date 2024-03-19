28745de4378acaeedd5d4ad86d24e9f2fdef3f33
Beelzebub <bleezbub12@gmail.com>
2024-03-19 12:01:58 +0530

    update and delete are complete T & W

--- Backend/routs/users.js
+++ Backend/routs/users.js
@@ -35,16 +35,61 @@ router.route("/").get((req, res) => {
 });
 
 router.route("/get/:id").get(async (req, res) => {
-  let userId = req.params.id;
-  const user = await User.findById(userId)
-    .then((user) => {
-      res
-        .status(200)
-        .json({ message: " User fetched successfully ", User: user });
-    })
-    .catch((err) => {
-      res.status(400).json({ message: ` User fetching unsuccessful ${err}` });
+  try {
+    const userId = req.params.id;
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    return res
+      .status(200)
+      .json({ message: "User fetched successfully", User: user });
+  } catch (err) {
+    return res
+      .status(400)
+      .json({ message: `User fetching unsuccessful ${err}` });
+  }
+});
+
+router.route("/:id").put(async (req, res) => {
+  try {
+    const uid = req.params.id;
+    const { Fname, Lname, Phone, Email, Dob } = req.body;
+
+    const result = await User.findByIdAndUpdate(uid, {
+      Fname,
+      Lname,
+      Phone,
+      Email,
+      Dob,
     });
+
+    if (!result) {
+      return res.status(404).json({ message: " User not found " });
+    }
+    return res.status(200).json({ message: " User update successful " });
+  } catch (err) {
+    return res.status(400).json({ message: `User update unsuccessful ${err}` });
+  }
+});
+
+router.route("/:id").delete(async (req, res) => {
+  try {
+    const uid = req.params.id;
+
+    const result = await User.findByIdAndDelete(uid);
+    if (!result) {
+      return res.status(404).json({ message: " User not found " });
+    }
+
+    return res.status(200).json({ message: " User deleted successfully " });
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ message: ` User deleted unsuccessfully ${error}` });
+  }
 });
 
 module.exports = router;
