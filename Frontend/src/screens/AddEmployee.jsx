6f3e3c5dfe94aeea38ccf62ac0aa1c5e9670d476
it22145284 <it22145284@my.sliit.lk>
2024-04-25 11:43:51 +0530

    up

--- Frontend/src/screens/AddEmployee.jsx
+++ Frontend/src/screens/AddEmployee.jsx
@@ -11,7 +11,7 @@ const AddEmployee = () => {
 
   return (
     <div className="flex h-full justify-center items-center" style={bgStyle}>
-      <div className="bg-black/45 w-9/10 rounded-[50px] py-12 px-14 flex flex-col gap-y-8">
+      <div className="bg-black/45 w-1/2 rounded-[50px] py-12 px-14 flex flex-col gap-y-8">
         <div className="flex flex-row items-center justify-between gap-x-20">
           <p className="text-4xl text-white font-bold">Employee Register</p>
         </div>
