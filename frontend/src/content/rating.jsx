c33f02eb244a501f120defd54b39b975d4960248
Praboth Sandaruwan <bleezbub12@gmail.com>
2024-05-02 16:42:10 +0530

    gg

--- frontend/src/content/rating.jsx
+++ frontend/src/content/rating.jsx
@@ -1,27 +1,61 @@
-import React from 'react'
-import { FaStar,FaStarHalfAlt,FaRegStar } from 'react-icons/fa'
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { FaStar, FaStarHalfAlt, FaRegStar } from "react-icons/fa";
 
-const rating = ({value,text}) => {
+const rating = ({ value, text }) => {
   return (
-    <div className='rating'>
-        <span>
-            {value >= 1 ? <FaStar/> : value >= 0.5 ? <FaStarHalfAlt/> : <FaRegStar/>}
-        </span>
-        <span>
-            {value >= 2 ? <FaStar/> : value >= 1.5 ? <FaStarHalfAlt/> : <FaRegStar/>}
-        </span>
-        <span>
-            {value >= 3 ? <FaStar/> : value >= 2.5 ? <FaStarHalfAlt/> : <FaRegStar/>}
-        </span>
-        <span>
-            {value >= 4 ? <FaStar/> : value >= 3.5 ? <FaStarHalfAlt/> : <FaRegStar/>}
-        </span>
-        <span>
-            {value >= 5 ? <FaStar/> : value >= 4.5 ? <FaStarHalfAlt/> : <FaRegStar/>}
-        </span>
-        <span className='rating-text'> {text && text }</span>
+    <div className="rating">
+      <Row className="mt-3">
+        <Col md={1}>
+          {value >= 1 ? (
+            <FaStar />
+          ) : value >= 0.5 ? (
+            <FaStarHalfAlt />
+          ) : (
+            <FaRegStar />
+          )}
+        </Col>
+        <Col md={1}>
+          {value >= 2 ? (
+            <FaStar />
+          ) : value >= 1.5 ? (
+            <FaStarHalfAlt />
+          ) : (
+            <FaRegStar />
+          )}
+        </Col>
+        <Col md={1}>
+          {value >= 3 ? (
+            <FaStar />
+          ) : value >= 2.5 ? (
+            <FaStarHalfAlt />
+          ) : (
+            <FaRegStar />
+          )}
+        </Col>
+        <Col md={1}>
+          {value >= 4 ? (
+            <FaStar />
+          ) : value >= 3.5 ? (
+            <FaStarHalfAlt />
+          ) : (
+            <FaRegStar />
+          )}
+        </Col>
+        <Col md={1}>
+          {value >= 5 ? (
+            <FaStar />
+          ) : value >= 4.5 ? (
+            <FaStarHalfAlt />
+          ) : (
+            <FaRegStar />
+          )}
+        </Col>
+        <Col md={3}></Col>
+        <Col md={4}> {text && text}</Col>
+      </Row>
     </div>
-  )
-}
+  );
+};
 
-export default rating
+export default rating;
