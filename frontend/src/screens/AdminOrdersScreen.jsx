076989c3b19308644387398b15e554c009021e26
Praboth Sandaruwan <bleezbub12@gmail.com>
2024-05-05 23:33:49 +0530

    cmm bdg + rep

--- frontend/src/screens/AdminOrdersScreen.jsx
+++ frontend/src/screens/AdminOrdersScreen.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+//import { useSelector } from "react-redux";
 import {
   useGetAllOrdersQuery,
   useRemoveOrderMutation,
@@ -26,15 +26,14 @@ const AdminOrdersScreen = () => {
         console.log("Error occurred while deleting the order:", error);
       }
     }
-  }
+  };
 
   if (isLoading || isRemoving) {
     return <Loader />;
   }
 
-
   if (isError) {
-    console.log("Error occurred while fetching data:", isError); // Log the error
+    console.log("Error occurred while fetching data:", isError);
     return <div>Error occurred while fetching data</div>;
   }
 
@@ -42,23 +41,54 @@ const AdminOrdersScreen = () => {
     return <div>No orders found</div>;
   }
 
+  function convertToCSV(headers, data) {
+    const headerRow = headers.join(",");
+    const rows = data
+      .map((item) => headers.map((header) => item[header]).join(","))
+      .join("\n");
+    return `${headerRow}\n${rows}`;
+  }
+
+  const reportHandler = () => {
+    const headers = ["_id", "user", "totalPrice", "createdAt"];
+
+    const csvContent = convertToCSV(headers, orders);
+    const blob = new Blob([csvContent], { type: "text/csv" });
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "report.csv";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+
+    console.log("generated report");
+  };
+
+  const unpaidOrdersCount = orders.filter((order) => !order.isPaid).length;
+  const unDeliveredOrdersCount = orders.filter(
+    (order) => !order.isDelivered
+  ).length;
+
   return (
     <>
-      <div class="container mx-auto px-4 py-8">
-        <div class="flex justify-between items-center mb-6">
-          <div class="relative">
+      <div className="container mx-auto px-4 py-8">
+        <div className="flex justify-between items-center mb-6">
+          <div className="relative">
             <input
               type="text"
               placeholder="Search..."
-              class="border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:ring focus:border-blue-300 w-64"
+              className="border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:ring focus:border-blue-300 w-64"
             />
-            <button class="absolute right-0 top-0 mt-2 mr-2">
+            <button className="absolute right-0 top-0 mt-2 mr-2">
               <svg
-                class="h-5 w-5 text-gray-400"
+                className="h-5 w-5 text-gray-400"
                 fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
               >
@@ -67,111 +97,121 @@ const AdminOrdersScreen = () => {
             </button>
           </div>
 
-          <div class="space-x-4">
-            <button class="bg-green-400 hover:bg-green-500 rounded-lg h-10 w-36 text-center">
+          <div className="space-x-4">
+            <button className="bg-green-400 hover:bg-green-500 rounded-lg h-10 w-36 text-center">
               Generate Report
             </button>
-            <button class="bg-blue-400 hover:bg-blue-500 rounded-lg h-10 w-36 text-center">
+            <button
+              className="bg-blue-400 hover:bg-blue-500 rounded-lg h-10 w-36 text-center"
+              onClick={(e) => reportHandler()}
+            >
               Generate Report
             </button>
           </div>
         </div>
 
-        <div class="overflow-x-auto">
-          <div class="min-w-full">
-            <div class="shadow-md overflow-hidden border-b border-gray-200 sm:rounded-lg">
-              <table class="min-w-full divide-y divide-gray-200">
-                <thead class="bg-gray-50">
+        <div className="overflow-x-auto">
+          <div className="min-w-full">
+            <div className="shadow-md overflow-hidden border-b border-gray-200 sm:rounded-lg">
+              <table className="min-w-full divide-y divide-gray-200">
+                <thead className="bg-gray-50">
                   <tr>
                     <th
                       scope="col"
-                      class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                     >
                       ID
                     </th>
                     <th
                       scope="col"
-                      class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                     >
                       Customer
                     </th>
                     <th
                       scope="col"
-                      class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                     >
                       Date and Time
                     </th>
                     <th
                       scope="col"
-                      class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                     >
                       Payment
+                      <span className="ml-1 bg-red-500 text-white px-2 py-1 rounded-full">
+                        {unpaidOrdersCount}
+                      </span>
                     </th>
                     <th
                       scope="col"
-                      class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                     >
-                      Delivary
+                      Delivery
+                      <span className="ml-1 bg-red-500 text-white px-2 py-1 rounded-full">
+                        {unDeliveredOrdersCount}
+                      </span>
                     </th>
                     <th
                       scope="col"
-                      class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                     >
                       Action
                     </th>
                   </tr>
                 </thead>
-                <tbody class="bg-white divide-y divide-gray-200">
+                <tbody className="bg-white divide-y divide-gray-200">
                   {orders.map(
                     (order) => (
+                      //console.log(order),
                       (
-                        <tr>
-                          <td class="px-6 py-4 whitespace-nowrap">
+                        <tr key={order._id}>
+                          <td className="px-6 py-4 whitespace-nowrap">
                             {order._id}
                           </td>
-                          <td class="px-6 py-4 whitespace-nowrap">
+                          <td className="px-6 py-4 whitespace-nowrap">
                             {order.user}
                           </td>
-                          <td class="px-6 py-4 whitespace-nowrap">{`${order.createdAt.slice(
+                          <td className="px-6 py-4 whitespace-nowrap">{`${order.createdAt.slice(
                             0,
                             10
                           )} at ${order.createdAt.slice(11, 19)}`}</td>
-                          <td class="px-6 py-4 whitespace-nowrap">
+                          <td className="px-6 py-4 whitespace-nowrap">
                             {order.isPaid ? (
-                              <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                              <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
                                 Paid
                               </span>
                             ) : (
-                              <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">
+                              <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">
                                 Not Paid
                               </span>
                             )}
                           </td>
-                          <td class="px-6 py-4 whitespace-nowrap">
+                          <td className="px-6 py-4 whitespace-nowrap">
                             {order.isDelivered ? (
-                              <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                              <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
                                 Delivered
                               </span>
                             ) : (
-                              <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">
+                              <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">
                                 Not Delivered
                               </span>
                             )}
                           </td>
-                          <td class="px-6 py-4 whitespace-nowrap">
+                          <td className="px-6 py-4 whitespace-nowrap">
                             <button
-                              class="ml-2 px-4 py-2 font-medium text-white bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue active:bg-blue-600 transition duration-150 ease-in-out"
+                              className="ml-2 px-4 py-2 font-medium text-white bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue active:bg-blue-600 transition duration-150 ease-in-out"
                               onClick={() =>
                                 navigate(`/store/orderst/${order._id}`)
                               }
                             >
                               View
                             </button>
-                            <button class="ml-2 px-4 py-2 font-medium text-white bg-yellow-600 rounded-md hover:bg-yellow-500 focus:outline-none focus:shadow-outline-blue active:bg-yellow-600 transition duration-150 ease-in-out">
+                            <button className="ml-2 px-4 py-2 font-medium text-white bg-yellow-600 rounded-md hover:bg-yellow-500 focus:outline-none focus:shadow-outline-blue active:bg-yellow-600 transition duration-150 ease-in-out">
                               Edit
                             </button>
                             <button
-                              class="ml-2 px-4 py-2 font-medium text-white bg-red-600 rounded-md hover:bg-red-500 focus:outline-none focus:shadow-outline-red active:bg-red-600 transition duration-150 ease-in-out"
+                              className="ml-2 px-4 py-2 font-medium text-white bg-red-600 rounded-md hover:bg-red-500 focus:outline-none focus:shadow-outline-red active:bg-red-600 transition duration-150 ease-in-out"
                               onClick={(e) => deleteOrderHandler(order._id)}
                             >
                               Delete
