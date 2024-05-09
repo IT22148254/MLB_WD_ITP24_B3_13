c298623703f180e2186bed08ff24559153520a48
RaveenRanaweera <it22606488@my.sliit.lk>
2024-05-09 22:51:45 +0530

    create change Time slot

---
+++ frontend/src/Pages/OM/SceduleTimeSlot.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { Container } from "reactstrap";
+import bgImage from "../../Images/bg.jpg";
+import TimeSlotImage from "..//../Images/TimeSlot.jpg";
+import logo from "../../Images/wlogo.png";
+import buttonImage1 from "../../Images/ed.png";
+import buttonImage2 from "../../Images/el.png";
+import buttonImage3 from "../../Images/ic.png";
+import buttonImage4 from "../../Images/pr.png";
+
+
+const SceduleTimeSlot = () => {
+  return (
+    <div className="relative h-screen" style={{ backgroundImage: `url(${bgImage})`, backgroundSize: 'cover' }}>
+      <div className="relative flex items-center justify-center w-2/3 h-full mx-auto">
+        <img src={TimeSlotImage} alt="Time Slot" />
+        <div className="absolute top-10 right-0 flex items-center bottom-[400px] left-[180px]">
+          <img src={logo} alt="logo" className="h-[80px] w-[100px]" />
+          <div className="absolute bottom-0 left-[25px] right-0 flex items-center top-[100px]">
+          <p className="text-[10px] text-white" style={{ fontFamily: 'poppins' }}>wavesync</p>
+        </div>
+        </div>
+        <div className="absolute top-0 right-0 flex items-center left-[140px] bottom-20">
+          <p className="text-2xl text-white "  style={{ fontFamily: 'poppins' }}>Welcome Back!</p>
+        </div>
+        <section>
+  <div className="absolute top-0 bottom-[400px] right-0 flex items-center left-[650px]">
+    <button className="flex flex-col items-center">
+      <img src={buttonImage1} alt="button" className="h-[200px] w-[200px]" />
+      <p className="absolute text-xl bottom-[120px] left-0 right-[450px] text-center text-white font-normal" style={{ fontFamily: 'poppins' }}>
+        Employee Details
+      </p>
+    </button>
+  </div>
+
+  <div className="absolute top-0 bottom-[400px] right-0 flex items-center left-[1000px]">
+    <button className="flex flex-col items-center">
+      <img src={buttonImage2} alt="button" className="h-[200px] w-[200px]" />
+      <p className="absolute text-xl bottom-[122px] left-0 right-[25px] text-white font-normal" style={{ fontFamily: 'poppins' }}>
+        Employee Leave Request
+      </p>
+    </button>
+  </div>
+
+  <div className="absolute top-[650px] bottom-[380px] right-0 flex items-center left-[650px]">
+    <button className="flex flex-col items-center">
+      <img src={buttonImage3} alt="button" className="h-[200px] w-[200px]" />
+      <p className="absolute text-xl top-[110px] bottom-0 left-0 right-[450px] text-center text-white font-normal" style={{ fontFamily: 'poppins' }}>
+        Instructor Change Request
+      </p>
+    </button>
+  </div>
+
+  <div className="absolute top-[650px] bottom-[380px] right-0 flex items-center left-[1000px]">
+    <button className="flex flex-col items-center">
+      <img src={buttonImage4} alt="button" className="h-[200px] w-[200px]" />
+      <p className="absolute text-xl bottom-0 top-[110px] left-0 right-[120px] text-center text-white font-normal" style={{ fontFamily: 'poppins' }}>
+        Promo Package Request
+      </p>
+    </button>
+  </div>
+</section>
+        
+      </div>
+    </div>
+  );
+};
+
+export default SceduleTimeSlot;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,6 +13,8 @@ import TestScreen from './Components/Membership/Manager/TestScreen';
 import ChangeTimeOnDayForm from './Components/Membership/Manager/ChangeOnetimeForm';
 import OnetimeChangeTable from './Components/Membership/Manager/OnetimeChangeTable';
 import ChangeOnetimeEdit from './Components/Membership/Manager/ChangeOnetimeEdit';
+import SceduleTimeSlot from "../src/Pages/OM/SceduleTimeSlot";
+
 import { ToastContainer } from 'react-toastify';
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -23,9 +25,11 @@ const router = createBrowserRouter(
         {/* <Route path='chngtimeondy/' element={<ChangeTimeOnDy />} /> */}
         <Route path='chngtimeondytbl/' element={<OnetimeChangeTable/>}/>
         <Route path='chngtimeondytbl/:id'element={<ChangeOnetimeEdit/>}/>
+        <Route path='schedule/'element={<SceduleTimeSlot/>}/>
         {/* <Route path='addnewpr/' element={<AddNewPromoForm />} />
         <Route path='createdpromos/' element={<CreatedPromos />} />
-        <Route path='editpromo/:id' element={<EditPromoForm/>} />
+        <Route path='
+        ....................................................................................................................................................................................................................................................................................................................................................................' element={<EditPromoForm/>} />
         <Route path="editstandard/:id" element={<EditStandard />} />
         <Route path="testscreen/" element={<TestScreen />} /> */}
       </Route>
