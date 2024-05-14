1ddd8a2e1f8f0a817346b2d43144fd891d009734
it22145284 <it22145284@my.sliit.lk>
2024-05-14 12:05:02 +0530

    up

--- Backend/models/feedback.js
+++ Backend/models/feedback.js
@@ -43,10 +43,55 @@ const coachFeedBackSchema = new schema({
   },
 });
 
+
+const approveFeedBackSchema = new schema({
+  UserName: {
+    type: String,
+    required: true,
+  },
+  Coach: {
+    type: String,
+    required: true,
+  },
+  Comment: {
+    type: String,
+    required: true,
+  },
+  Rating: {
+    type: Number,
+    required: true,
+  },
+  Email: {
+    type: String,
+    required: true,
+  },
+});
+
+
+const saFeedBackSchema = new schema({
+  UserName: {
+    type: String,
+    required: true,
+  },
+  Comment: {
+    type: String,
+    required: true,
+  },
+  Rating: {
+    type: Number,
+    required: true,
+  },
+  Email: {
+    type: String,
+    required: true,
+  },
+});
+
 const ServiceFeedBack = mongoose.model(
   "ServiceFeedBack",
   serviceFeedBackSchema
 );
 const CoachFeedBack = mongoose.model("CoachFeedBack", coachFeedBackSchema);
-
-module.exports = { ServiceFeedBack, CoachFeedBack };
+const ApproveFeedBack = mongoose.model("ApproveFeedBack", approveFeedBackSchema);
+const SaFeedBack = mongoose.model("Sa", saFeedBackSchema);
+module.exports = { ServiceFeedBack, CoachFeedBack ,ApproveFeedBack,SaFeedBack};
