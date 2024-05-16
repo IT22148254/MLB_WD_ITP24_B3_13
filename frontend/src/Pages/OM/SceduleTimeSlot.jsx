cf08a6eb9783aac5cc6b05b3c3a4c639cd78df43
Praboth Sandaruwan <bleezbub12@gmail.com>
2024-05-16 12:04:24 +0530

    cmm

--- frontend/src/Pages/OM/SceduleTimeSlot.jsx
+++ frontend/src/Pages/OM/SceduleTimeSlot.jsx
@@ -7,9 +7,11 @@ import buttonImage1 from "../../Images/ed.png";
 import buttonImage2 from "../../Images/el.png";
 import buttonImage3 from "../../Images/ic.png";
 import buttonImage4 from "../../Images/pr.png";
+import { useNavigate } from "react-router-dom";
 
 
 const SceduleTimeSlot = () => {
+  const navigate = useNavigate()
   return (
     <div className="relative h-screen" style={{ backgroundImage: `url(${bgImage})`, backgroundSize: 'cover' }}>
       <div className="relative flex items-center justify-center w-2/3 h-full mx-auto">
@@ -43,7 +45,7 @@ const SceduleTimeSlot = () => {
   </div>
 
   <div className="absolute top-[650px] bottom-[380px] right-0 flex items-center left-[650px]">
-    <button className="flex flex-col items-center">
+    <button className="flex flex-col items-center" onClick={()=>{navigate("/sch/change")}}>
       <img src={buttonImage3} alt="button" className="h-[200px] w-[200px]" />
       <p className="absolute text-xl top-[110px] bottom-0 left-0 right-[450px] text-center text-white font-normal" style={{ fontFamily: 'poppins' }}>
         Instructor Change Request
@@ -52,10 +54,10 @@ const SceduleTimeSlot = () => {
   </div>
 
   <div className="absolute top-[650px] bottom-[380px] right-0 flex items-center left-[1000px]">
-    <button className="flex flex-col items-center">
+    <button className="flex flex-col items-center" onClick={()=>{navigate("/sch/addsch")}}>
       <img src={buttonImage4} alt="button" className="h-[200px] w-[200px]" />
       <p className="absolute text-xl bottom-0 top-[110px] left-0 right-[120px] text-center text-white font-normal" style={{ fontFamily: 'poppins' }}>
-        Promo Package Request
+        Add schedule
       </p>
     </button>
   </div>
